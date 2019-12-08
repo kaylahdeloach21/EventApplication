1042b97a9a29566bb39404907a73ae8792e3b35b
Kaylah DeLoach <46730990+kaylahdeloach21@users.noreply.github.com>
2019-12-08 13:22:41 -0500

    Add files via upload

---
+++ EventApplication/Scripts/Apps/OrderScripts.js
@@ -0,0 +1,17 @@
+﻿$(function () {
+    $(".CancelLink").click(function () {
+        var id = $(this).attr("data-id");
+
+        $.post("/TicketsOrdered/CancelOrder", { "id": id }, function (data) {
+            $("#item-status-" + data.DeleteId).text(data.Status);
+            //$("#update-message").text(data.Status);
+            //document.getElementById("link").innerHTML = "";
+            //$("#cart-total").text(data.CartTotal);
+            //$("#item-count-" + data.DeleteId).text(data.ItemCount);
+
+            //if (data.ItemCount < 1) {
+            //    $("#record-" + data.DeleteId).fadeOut();
+            //}
+        });
+    });
+});
