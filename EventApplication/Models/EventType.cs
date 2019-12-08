d90d56c0299c284c925042c3eccea67a848e418d
Kaylah DeLoach <46730990+kaylahdeloach21@users.noreply.github.com>
2019-12-08 13:21:25 -0500

    Add files via upload

---
+++ EventApplication/Models/EventType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EventApplication.Models
+{
+    public class EventType
+    {
+        public virtual int EventTypeId { get; set; }
+        [Required(ErrorMessage = "{0} is required")]
+        [StringLength(50, ErrorMessage = "Only 50 characters allowed")]
+        public virtual string Type { get; set; }
+    }
+}
